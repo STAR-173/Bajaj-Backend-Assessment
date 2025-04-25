cc8b6ab6bc7ab03be3fef5b6fe3097e87fd686e8
Shoaib <starvoltz173@gmail.com>
2025-04-25 17:25:56 +0530

    excluded auto-db initialization

--- src/main/java/com/star/bajaj_assesment/BajajAssesmentApplication.java
+++ src/main/java/com/star/bajaj_assesment/BajajAssesmentApplication.java
@@ -2,8 +2,9 @@ package com.star.bajaj_assesment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class BajajAssesmentApplication {
 
 	public static void main(String[] args) {

c9aa3bc69e4277ba846b976908b135fd74e4a24d
Shoaib <starvoltz173@gmail.com>
2025-04-25 17:05:49 +0530

    Initialized

---
+++ src/main/java/com/star/bajaj_assesment/BajajAssesmentApplication.java
@@ -0,0 +1,13 @@
+package com.star.bajaj_assesment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BajajAssesmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BajajAssesmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/star/bajaj_assesment/BajajAssesmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.bajaj_assesment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BajajAssesmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
